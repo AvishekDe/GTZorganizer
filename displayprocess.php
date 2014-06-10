3d238d44d28c363a532f015332e1d9ac7eb76fc7
AvishekDe <avishekde.iitr@gmail.com>
2014-06-10 22:02:00 +0530

    Added support for displaying points table for all groups at once

--- displayprocess.php
+++ displayprocess.php
@@ -4,54 +4,115 @@
 
 	$con = mysqli_connect("localhost" , "root" , "password" , "GTZorganizer");
 
-	$result = mysqli_query($con , "SELECT rank FROM central WHERE grp='$gname'");
+	
+
+	if($gname == 'X')
+	{
+		for($j = 'A' ; $j <= 'H' ; $j++)
+		{
+			$gname = $j ;
+			$result = mysqli_query($con , "SELECT rank FROM central WHERE grp='$gname'");
+				while($row = mysqli_fetch_array($result)){
+					$arr[$i] = $row['rank'];
+					$i++;
+				}
+
+				//Sorting members
+				$sql = mysqli_query($con , "SELECT * FROM central WHERE grp='$gname' ORDER BY points DESC , gd DESC , gf DESC , gfa DESC");
+
+				// Displaying tables
+				?>
+				<html>
+					<body>
+						<h1>Group &nbsp<?php echo $gname; ?></h1>
+						<table border="1" style="text-align:center; padding: 1px;">
+							<tr>
+							<th> Rank </th>
+							<th> Display Name</th>
+							<th> Full Name</th>
+							<th> Team Name</th>
+							<th> Played</th>
+							<th> Won </th>
+							<th> Drew </th>
+							<th> Lost </th>
+							<th> GD </th>
+							<th> Points </th>
+
+							</tr>
+						
+					<?php
+					while ($row = mysqli_fetch_array($sql))
+					{
+						echo "<tr>";
+						echo "<td>" . $row['rank'] . "</td>";
+						echo "<td>" . $row['dname'] . "</td>";
+						echo "<td>" . $row['fname'] . "</td>";
+						echo "<td>" . $row['team'] . "</td>";
+						echo "<td>" . $row['played'] . "</td>";
+						echo "<td>" . $row['won'] . "</td>";
+						echo "<td>" . $row['drew'] . "</td>";
+						echo "<td>" . $row['lost'] . "</td>";
+						echo "<td>" . $row['gd'] . "</td>";
+						echo "<td>" . $row['points'] . "</td>";
+						echo "</tr>";
+					}
+				?>
+				</table>
+					</body>
+				</html>
+	<?php }
+		}
 
 	// Fetching group members
-	while($row = mysqli_fetch_array($result)){
-		$arr[$i] = $row['rank'];
-		$i++;
-	}
+	else
+	{
+				$result = mysqli_query($con , "SELECT rank FROM central WHERE grp='$gname'");
+				while($row = mysqli_fetch_array($result)){
+					$arr[$i] = $row['rank'];
+					$i++;
+				}
 
-	//Sorting members
-	$sql = mysqli_query($con , "SELECT * FROM central WHERE grp='$gname' ORDER BY points DESC , gd DESC , gf DESC , gfa DESC");
+				//Sorting members
+				$sql = mysqli_query($con , "SELECT * FROM central WHERE grp='$gname' ORDER BY points DESC , gd DESC , gf DESC , gfa DESC");
 
-	// Displaying tables
-	?>
-	<html>
-		<body>
-			<h1>Group &nbsp<?php echo $gname; ?></h1>
-			<table border="1" style="text-align:center; padding: 1px;">
-				<tr>
-				<th> Rank </th>
-				<th> Display Name</th>
-				<th> Full Name</th>
-				<th> Team Name</th>
-				<th> Played</th>
-				<th> Won </th>
-				<th> Drew </th>
-				<th> Lost </th>
-				<th> Points </th>
-				<th> GD </th>
+				// Displaying tables
+				?>
+				<html>
+					<body>
+						<h1>Group &nbsp<?php echo $gname; ?></h1>
+						<table border="1" style="text-align:center; padding: 1px;">
+							<tr>
+							<th> Rank </th>
+							<th> Display Name</th>
+							<th> Full Name</th>
+							<th> Team Name</th>
+							<th> Played</th>
+							<th> Won </th>
+							<th> Drew </th>
+							<th> Lost </th>
+							<th> GD </th>
+							<th> Points </th>
 
-				</tr>
-			
-		<?php
-		while ($row = mysqli_fetch_array($sql))
-		{
-			echo "<tr>";
-			echo "<td>" . $row['rank'] . "</td>";
-			echo "<td>" . $row['dname'] . "</td>";
-			echo "<td>" . $row['fname'] . "</td>";
-			echo "<td>" . $row['team'] . "</td>";
-			echo "<td>" . $row['played'] . "</td>";
-			echo "<td>" . $row['won'] . "</td>";
-			echo "<td>" . $row['drew'] . "</td>";
-			echo "<td>" . $row['lost'] . "</td>";
-			echo "<td>" . $row['points'] . "</td>";
-			echo "<td>" . $row['gd'] . "</td>";
-			echo "</tr>";
-		}
-	?>
-	</table>
-		</body>
-	</html>
+							</tr>
+						
+					<?php
+					while ($row = mysqli_fetch_array($sql))
+					{
+						echo "<tr>";
+						echo "<td>" . $row['rank'] . "</td>";
+						echo "<td>" . $row['dname'] . "</td>";
+						echo "<td>" . $row['fname'] . "</td>";
+						echo "<td>" . $row['team'] . "</td>";
+						echo "<td>" . $row['played'] . "</td>";
+						echo "<td>" . $row['won'] . "</td>";
+						echo "<td>" . $row['drew'] . "</td>";
+						echo "<td>" . $row['lost'] . "</td>";
+						echo "<td>" . $row['gd'] . "</td>";
+						echo "<td>" . $row['points'] . "</td>";
+						echo "</tr>";
+					}
+				?>
+				</table>
+					</body>
+				</html>
+<?php } ?>
