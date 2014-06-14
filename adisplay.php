104ccfe74849ee555721bd4106625ed09b53296c
AvishekDe <avishekde.iitr@gmail.com>
2014-06-14 12:18:09 +0530

    Added display support for top goals scorers and assists

---
+++ adisplay.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+</head>
+<body>
+	<h2>List of Assists:</h2><br>
+	<table border="1">
+		<tr>
+			<th>Rank</th>
+			<th>Display Name</th>
+			<th>Player Name</th>
+			<th>Assists</th>
+		</tr>
+	
+
+<?php
+	$i = 1 ;
+	$con = mysqli_connect("localhost" , "root" , "password" , "GTZorganizer");
+
+	$result = mysqli_query($con , "SELECT * FROM assists ORDER BY tassists DESC");
+
+	while($row = mysqli_fetch_array($result))
+	{
+		echo "<tr>";
+		echo "<th>" . $i . "</th>";
+		echo "<th>" . $row['dname'] . "</th>";
+		echo "<th>" . $row['player'] . "</th>";
+		echo "<th>" . $row['tassists'] . "</th>";
+		echo "</tr>";
+		$i++;
+	}
+?>
+
+
+
+</table>
+<br><br>
+		<a href="home.php"><button id="hbut">Admin-Home</button></a>
+		<br>
+		<a href="homeg.php"><button id="hbut">Guest-Home</button></a>
+</body>
+</html>
---
+++ gdisplay.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+</head>
+<body>
+	<h2>List of Goal Scorers:</h2><br>
+	<table border="1">
+		<tr>
+			<th>Rank</th>
+			<th>Display Name</th>
+			<th>Player Name</th>
+			<th>Goals</th>
+		</tr>
+	
+
+<?php
+	$i = 1 ;
+	$con = mysqli_connect("localhost" , "root" , "password" , "GTZorganizer");
+
+	$result = mysqli_query($con , "SELECT * FROM goals ORDER BY tgoals DESC");
+
+	while($row = mysqli_fetch_array($result))
+	{
+		echo "<tr>";
+		echo "<th>" . $i . "</th>";
+		echo "<th>" . $row['dname'] . "</th>";
+		echo "<th>" . $row['player'] . "</th>";
+		echo "<th>" . $row['tgoals'] . "</th>";
+		echo "</tr>";
+		$i++;
+	}
+?>
+
+
+
+</table>
+<br><br>
+		<a href="home.php"><button id="hbut">Admin-Home</button></a>
+		<br>
+		<a href="homeg.php"><button id="hbut">Guest-Home</button></a>
+</body>
+</html>
