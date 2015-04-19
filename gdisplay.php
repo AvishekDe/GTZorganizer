9c8c21c907957706c41ec3578ac486add6fb3036
AvishekDe <avishekde.iitr@gmail.com>
2015-04-20 02:37:59 +0530

    Added CSS style for top scorers

--- gdisplay.php
+++ gdisplay.php
@@ -3,8 +3,8 @@
 <link rel="stylesheet" type="text/css" href="./css/main.css">
 </head>
 <body>
-	<h2>List of Goal Scorers:</h2><br>
-	<table border="1">
+	<div class="options-title">List of Goal Scorers:</div>
+	<table class="display-tables" border="1">
 		<tr class="h">
 			<th>Rank</th>
 			<th>Display Name</th>
@@ -17,16 +17,19 @@
 	$i = 1 ;
 	$conf = parse_ini_file("config.ini");
 	$con = mysqli_connect($conf['host'] , $conf['user'] , $conf['password'] , $conf['database']);
-
+	$count = 0;
 	$result = mysqli_query($con , "SELECT * FROM goals ORDER BY tgoals DESC");
 
 	while($row = mysqli_fetch_array($result))
 	{
-		echo "<tr>";
-		echo "<th>" . $i . "</th>";
-		echo "<th>" . $row['dname'] . "</th>";
-		echo "<th>" . $row['player'] . "</th>";
-		echo "<th>" . $row['tgoals'] . "</th>";
+		$count++;
+		$rowmarker = (int)($count%2);
+		if($rowmarker == 0) $rowmarker = "even";
+		echo "<tr class='$rowmarker'>";
+		echo "<td>" . $i . "</td>";
+		echo "<td>" . $row['dname'] . "</td>";
+		echo "<td>" . $row['player'] . "</td>";
+		echo "<td>" . $row['tgoals'] . "</td>";
 		echo "</tr>";
 		$i++;
 	}
